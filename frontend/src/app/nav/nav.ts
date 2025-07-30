d7f30e331953563bbeda44a3c22b5773e0166f0a
Teo344 <gabriel2004mat@gmail.com>
2025-07-30 08:05:10 -0500

    Mejora responsiva del programa

--- frontend/src/app/nav/nav.ts
+++ frontend/src/app/nav/nav.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
+  standalone: true,
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  menuAbierto = false;
 
 }
