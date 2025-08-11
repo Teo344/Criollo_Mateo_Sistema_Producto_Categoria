baf9f07f125723e0ba316559e729b6a67226e421
Teo344 <gabriel2004mat@gmail.com>
2025-08-10 21:54:21 -0500

    subifa

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
+// export const environment = {
+//   production: true,
+//   apiUrlProducts: 'http://backend-products:8080/api/products',
+//   apiUrlCategories: 'http://backend-categories:8080/api/categories'
+// };
+
+
 export const environment = {
   production: true,
-  apiUrlProducts: 'http://backend-products:8080/api/products',
-  apiUrlCategories: 'http://backend-categories:8080/api/categories'
+  apiUrlProducts: '/api/products',
+  apiUrlCategories: '/api/categories'
 };
